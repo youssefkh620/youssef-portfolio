f9bacf3b4453b57964b4977de33f71e74afd9e7d
Youssef Khaled <yousefkh620@gmail.com>
2026-08-07 14:32:01 +0300

    Add timeline section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ import { IoMailOpenOutline } from "react-icons/io5";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#07111F] border border-t-[#38BDF8]/12 p-5 flex justify-between">
+    <footer className="bg-[#07111F] border border-t-[#38BDF8]/12 p-5 grid grid-cols-2 lg:grid-cols-3 items-center">
       <h1 className="text-cyan-400">
         &lt; <span className="text-white font-bold">Youssef Khaled</span> /&gt;
       </h1>
 
-      <div className="flex items-center gap-4 text-[#94A3B8]">
+      <div className="flex items-center gap-4 text-[#94A3B8] justify-end lg:justify-center ">
         <a
           href="https://github.com/youssefkh620"
           target="_blank"
@@ -30,7 +30,7 @@ export default function Footer() {
           <IoMailOpenOutline />
         </a>
       </div>
-      <p className="text-sm text-[#94A3B8]">
+      <p className="text-sm text-[#94A3B8]  lg:text-end col-span-2 lg:col-span-1 mt-2 lg:mt-0">
         © 2026<span className="font-extrabold"> Youssef Khaled. </span> All
         rights reserved.
       </p>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,56 @@
 export default function About() {
   return (
     <>
-      <h1>About</h1>
+      <TimeLine />
+    </>
+  );
+}
+
+function TimeLine() {
+  const timeline = [
+    {
+      year: "2022",
+      title: "Started University",
+      description: "Started studying Information Systems.",
+    },
+    {
+      year: "2025",
+      title: "ITI Certificate",
+      description: "Earned a certificate in Web Design Using Python from ITI.",
+    },
+    {
+      year: "2026",
+      title: "Graduation",
+      description: "Graduated with a Bachelor's degree in Information Systems.",
+    },
+  ];
+  return (
+    <>
+      <section className="py-20 px-5 bg-[#07111F]">
+        <h2 className="mb-12 text-start text-[#38BDF8] text-sm font-bold">
+          EDUCATION
+        </h2>
+        <div className="relative mx-auto max-w-3xl">
+          <div className="absolute left-4 top-0 w-1 h-full bg-linear-180 from-[#38BDF8]  to-transparent md:left-1/2 md:-translate-x-1/2" />
+          {timeline.map((item, index) => (
+            <div
+              key={index}
+              className="relative mb-12 flex items-start md:even:flex-row-reverse"
+            >
+              <div className="ml-10 w-full rounded-2xl border border-[#38BDF8]/12 bg-[#0F1D31] p-5 md:ml-0 md:w-[45%]">
+                <span className="text-sm font-semibold text-sky-400">
+                  {item.year}
+                </span>
+                <h3 className="mt-1 text-xl font-bold text-white">
+                  {item.title}
+                </h3>
+                <p className="mt-2 text-gray-400">{item.description}</p>
+              </div>
+              <div className="absolute left-4 top-5 h-3 w-3 -translate-x-1/2 rounded-full bg-[#38BDF8] ring-4 ring-[#38BDF8]/12 md:left-1/2"></div>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   );
 }
